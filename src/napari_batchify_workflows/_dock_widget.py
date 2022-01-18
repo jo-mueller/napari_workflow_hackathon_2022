b94ba0690a1ec54e552f86f5bc175eac5aee6ece
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-01-18 14:35:57 +0100

    added open workflow function to widget

--- src/napari_batchify_workflows/_dock_widget.py
+++ src/napari_batchify_workflows/_dock_widget.py
@@ -10,8 +10,15 @@ from napari_plugin_engine import napari_hook_implementation
 from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 from magicgui import magic_factory
 
+from skimage import io
 
-class ExampleQWidget(QWidget):
+import os
+import pickle
+import glob
+
+from qtpy import QtWidgets
+
+class WorkflowDispatcher(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -20,22 +27,41 @@ class ExampleQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        btn = QPushButton("Load workflow")
+        btn.clicked.connect(self._load_workflow)
+        btn.select_directory(self..)
 
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(btn)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+    def _select_data(self):
+        dirname = QtWidgets.QFileDialog.getExistingDirectory()
+        import glob
+        for f in glob.glob('/Users/fernandesm/Downloads/Wormhole P92eZ/211229/P2/CS1/*.tif', recursive=True):
+            print(f)
 
+    def _load_workflow(self):
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName()
+        with open(filename, "rb") as p:
+            self.workflow = pickle.load(p)
 
+    def _run_workflow(self, list_of_images: list):
+
+        for img in list_of_images:
+
+            image = io.imread(img)
+            directory = os.path.dirname(image)
+
+            outfile = os.path.join(directory,
+                                   os.path.basename(image).split('.')[0] + 'processed.tif')
+
+            self.workflow.set("Input", image)
+            result = self.workflow.get("labeled")
+
+            io.imwrite(result)
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
-    return [ExampleQWidget, example_magic_widget]
+    return [WorkflowDispatcher]
